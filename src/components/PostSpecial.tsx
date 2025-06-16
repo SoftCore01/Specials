49eac78f173a9248838725f693952fec6de7cc91
unknown <Omohunwa>
2025-06-16 10:44:22 +0100

    new changes

--- src/components/PostSpecial.tsx
+++ src/components/PostSpecial.tsx
@@ -1,106 +1,111 @@
-import { useRef } from "react"
-import { useFoodContext } from "../utils"
-import { useNavigate, Link } from "react-router-dom"
+import { useRef } from "react";
+import { useFoodContext } from "../utils";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function PostSpecial() {
-    const {specials, setSpecials} = useFoodContext()
-    const navigate = useNavigate();
+  const { specials, setSpecials } = useFoodContext();
+  const navigate = useNavigate();
 
-    const formRef = useRef<HTMLFormElement>(null)
-    
+  const formRef = useRef<HTMLFormElement>(null);
 
-    const postSpecial = () => {
-        const newSpecial = [...specials];
-        const formData = new FormData(formRef.current!);
-        newSpecial.push({
-          id: newSpecial[newSpecial.length - 1].id + 1,
-          restaurant: String(formData.get("restaurant")),
-          dish: String(formData.get("dish")),
-          originalPrice: parseInt(String(formData.get("price"))),
-          discount: parseInt(String(formData.get("discount"))),
-          quantity: parseInt(String(formData.get("quantity"))),
-          expiry: String(formData.get("expiry")),
-        });
-        setSpecials(newSpecial);
-        navigate('/')
-    }
+  const postSpecial = () => {
+    const newSpecial = [...specials];
+    const formData = new FormData(formRef.current!);
+    newSpecial.push({
+      id: newSpecial[newSpecial.length - 1].id + 1,
+      restaurant: String(formData.get("restaurant")),
+      dish: String(formData.get("dish")),
+      originalPrice: parseInt(String(formData.get("price"))),
+      discount: parseInt(String(formData.get("discount"))),
+      quantity: parseInt(String(formData.get("quantity"))),
+      expiry: String(formData.get("expiry")),
+    });
+    setSpecials(newSpecial);
+    navigate("/");
+  };
 
-    return (
-      <form
-        className="form-container"
-        ref={formRef}
-        onSubmit={(e) => {
-          e.preventDefault();
-          postSpecial();
-        }}
-      >
-        <h2>Post a Special</h2>
-        <label htmlFor="restaurant" className="form-label">
-          <input
-            className="form-input"
-            type="text"
-            name="restaurant"
-            id="restaurant"
-            placeholder="Restaurant name"
-            required
-          />
-        </label>
-        <label htmlFor="dish" className="form-label">
-          <input
-            className="form-input"
-            type="text"
-            name="dish"
-            id="dish"
-            placeholder="Dish name"
-            required
-          />
-        </label>
-        <label htmlFor="Price" className="form-label">
-          <input
-            className="form-input"
-            type="number"
-            name="price"
-            min={1}
-            id="price"
-            placeholder="Price"
-            required
-          />
-        </label>
-        <label htmlFor="discount" className="form-label">
-          <input
-            className="form-input"
-            type="number"
-            min={1}
-            name="discount"
-            id="discount"
-            placeholder="Discount percentage"
-            required
-          />
-        </label>
-        <label htmlFor="quantity" className="form-label">
-          <input
-            className="form-input"
-            type="number"
-            min={1}
-            name="quantity"
-            id="quantity"
-            placeholder="Quantity"
-            required
-          />
-        </label>
-        <label htmlFor="expiry" className="form-label">
-          <input
-            className="form-input"
-            type="time"
-            name="expiry"
-            id="expiry"
-            required
-          />
-        </label>
-        <div className="form-nav">
-          <button className="form-button">Enter Special</button>
-          <Link to="/">Go Home</Link>
-        </div>
-      </form>
-    );
+  return (
+    <form
+      className="form-container"
+      ref={formRef}
+      onSubmit={(e) => {
+        e.preventDefault();
+        postSpecial();
+      }}
+    >
+      <h2>Post a Special</h2>
+      <label htmlFor="restaurant" className="form-label">
+        Restaurant name:
+        <input
+          className="form-input"
+          type="text"
+          name="restaurant"
+          id="restaurant"
+          placeholder="e.g The Place"
+          required
+        />
+      </label>
+      <label htmlFor="dish" className="form-label">
+        DIsh name:
+        <input
+          className="form-input"
+          type="text"
+          name="dish"
+          id="dish"
+          placeholder="e.g Rice"
+          required
+        />
+      </label>
+      <label htmlFor="Price" className="form-label">
+        Price:
+        <input
+          className="form-input"
+          type="number"
+          min={1}
+          name="price"
+          id="price"
+          placeholder="e.g $50"
+          required
+        />
+      </label>
+      <label htmlFor="discount" className="form-label">
+        DIscount:
+        <input
+          className="form-input"
+          type="number"
+          min={1}
+          name="discount"
+          id="discount"
+          placeholder="e.g 20"
+          required
+        />
+      </label>
+      <label htmlFor="quantity" className="form-label">
+        Quantity:
+        <input
+          className="form-input"
+          type="number"
+          min={1}
+          name="quantity"
+          id="quantity"
+          placeholder="e.g 5"
+          required
+        />
+      </label>
+      <label htmlFor="expiry" className="form-label">
+        Expiry:
+        <input
+          className="form-input"
+          type="time"
+          name="expiry"
+          id="expiry"
+          required
+        />
+      </label>
+      <div className="form-nav">
+        <button className="form-button">Enter Special</button>
+        <Link to="/">Go Home</Link>
+      </div>
+    </form>
+  );
 }
--- src/components/ShowSpecial.tsx
+++ src/components/ShowSpecial.tsx
@@ -5,58 +5,54 @@ import { Link } from "react-router-dom";
 import ReservedCard from "./ReservedCard";
 
 export default function ShowSpecial() {
-    const {specials, reservations} = useFoodContext()
-    const [foods, setFood] = useState(specials)
+  const { specials, reservations } = useFoodContext();
+  const [foods, setFood] = useState(specials);
 
-    const handleFilter = (textInput: string) => {
-        if (textInput == "discount") {
-            const tempFoods = [...foods].sort(
-              (a, b) => a.discount - b.discount
-            );
-            setFood(tempFoods)
-        } else if (textInput == "price") {
-            const tempFoods = [...foods].sort(
-              (a, b) => a.originalPrice - b.originalPrice
-            );
-            setFood(tempFoods);
-        } else {
-            setFood(specials)
-        }
+  const handleFilter = (textInput: string) => {
+    if (textInput == "asc") {
+      const tempFoods = [...foods].sort((a, b) => a.discount - b.discount);
+      setFood(tempFoods);
+    } else if (textInput == "desc") {
+      const tempFoods = [...foods].sort((a, b) => b.discount - a.discount);
+      setFood(tempFoods);
+    } else {
+      setFood(specials);
     }
+  };
 
-    useEffect(() => setFood(specials),[specials])
+  useEffect(() => setFood(specials), [specials]);
 
-    return (
-      <>
-        <div className="filter">
-          <select
-            name="filter"
-            id="filter"
-            onChange={(e) => handleFilter(e.target.value)}
-          >
-            <option value="">Filter</option>
-            <option value="asc">Filter by </option>
-            <option value="desc">Filter by Price</option>
-          </select>
-          <Link to="/postspecial">Post New Specials</Link>
+  return (
+    <>
+      <div className="filter">
+        <select
+          name="filter"
+          id="filter"
+          onChange={(e) => handleFilter(e.target.value)}
+        >
+          <option value="">Filter</option>
+          <option value="asc">Filter by lowest discount</option>
+          <option value="desc">Filter by highest discount</option>
+        </select>
+        <Link to="/postspecial">Post New Specials</Link>
+      </div>
+      <div>
+        <h2>Specials Menu</h2>
+        <div className="meals">
+          {foods.map((food, i) => (
+            <FoodCard key={i} {...food} index={i} />
+          ))}
         </div>
-        <div>
-          <h2>Specials Menu</h2>
-          <div className="meals">
-            {foods.map((food, i) => (
-              <FoodCard key={i} {...food} index={i} />
-            ))}
-          </div>
-        </div>
+      </div>
 
-        <div>
-          <h2>Reserved</h2>
-          <div className="meals">
-            {reservations.map((food, i) => (
-              <ReservedCard key={i} index={i} {...food} />
-            ))}
-          </div>
+      <div>
+        <h2>Reserved</h2>
+        <div className="meals">
+          {reservations.map((food, i) => (
+            <ReservedCard key={i} index={i} {...food} />
+          ))}
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 }
